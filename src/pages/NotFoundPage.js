df87ca462b5c74e45ec1575f334dd6e324be796d
RatanaSenpai <createbysenpai@gmail.com>
2023-05-25 12:15:17 +0200

    Mise en place de la page 404

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
+import '../styles/notfoundpage.css';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            Erreur 404
+        <div className='notFound'>
+            <h1 className='notfound-title'>404</h1>
+            <p className='notfound-text'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="notfound-link">Retour à la page d'accueil</Link>
+            <Footer />
         </div>
     );
 };
